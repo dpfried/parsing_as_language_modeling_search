05921715936ac1e76c9e50a68584b9abceec55a1
Daniel Fried <dfried@berkeley.edu>
2017-01-16 15:43:56 -0800

    nbest progress

--- nbest_parse.py
+++ nbest_parse.py
@@ -1,11 +1,13 @@
 import fileinput
 from bllipparser import RerankingParser, Tree
+import sys
 
 if __name__ == '__main__':
   rrp = RerankingParser()
   parser = 'wsj/WSJ-PTB3/parser'
   rrp.load_parser_model(parser)
-  for line in fileinput.input():
+  for sent_count, line in enumerate(fileinput.input()):
+    sys.stderr.write("\r%s" % sent_count)
     tokens = Tree(line).tokens()
     nbest = rrp.parse(tokens)
     print len(nbest)
