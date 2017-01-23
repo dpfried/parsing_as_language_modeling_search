2f4e1d95764de2b06bb131f81887a9f143836e2c
Daniel Fried <dfried@berkeley.edu>
2017-01-23 15:36:58 -0800

    draft of beam search

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ __author__ = 'dfried'
 
 from collections import namedtuple
 import numpy as np
+import heapq
 
 def make_matching_brackets(word_to_id):
   pairs = {}
@@ -18,6 +19,17 @@ CLOSE_NT = 2
 ConsCell = namedtuple('ConsCell', 'car, cdr')
 
 BeamState = namedtuple('BeamState', 'prev_beam_state, lstm_state, prev_word_index, open_nt_stack, open_nt_count, cons_nt_count, action_count, prev_action_type, prev_action_index, term_count, score')
+
+def backchain_beam_state(final_beam_state):
+  action_indices = []
+  word_indices = []
+  beam_state = final_beam_state
+  while beam_state is not None:
+    action_indices.append(beam_state.prev_action_index)
+    if beam_state.prev_word_index is not None:
+      word_indices.append(beam_state.prev_word_index)
+    beam_state = beam_state.prev_beam_state
+  return list(reversed(action_indices)), list(reversed(word_indices))
 
 class BeamSearch(object):
   def __init__(self, session, model, word_to_id, max_open_nts=100, max_cons_nts=8):
@@ -41,6 +53,68 @@ class BeamSearch(object):
     self.max_open_nts = max_open_nts
     self.max_cons_nts = max_cons_nts
 
+  def score_actions(self, beam_state):
+    feed_dict = {
+      self.model.input_data: np.array([[beam_state.prev_action_index]])
+    }
+    for (state_c, state_h), (c, h) in zip(beam_state.lstm_state, self.model.initial_state):
+      feed_dict[c] = state_c
+      feed_dict[h] = state_h
+    fetches = [self.model.log_probs]
+    for c, h in self.model.final_state:
+      fetches.append(c)
+    res = self.session.run(fetches, feed_dict)
+    log_probs = res[0]
+    lstm_state_flat = res[1:] # [c1, h1, c2, h2...]
+    lstm_state = [lstm_state_flat[i:i+2] for i in range(0, len(lstm_state_flat), 2)]
+    new_state = beam_state._replace(lstm_state=lstm_state)
+    return log_probs, new_state
+
+  def choose_action(self, beam_state, action_type, nt_index, action_index, action_score, len_terms):
+    # set this to be the word generated if we are shifting
+    new_prev_word_index = None
+    new_open_nt_stack = beam_state.open_nt_stack
+    new_open_nt_count = beam_state.open_nt_count
+    new_cons_nt_count = beam_state.cons_nt_count
+    new_term_count = beam_state.term_count
+
+    if action_type == OPEN_NT:
+      new_open_nt_stack = ConsCell(nt_index, beam_state.open_nt_stack)
+      new_open_nt_count += 1
+      new_cons_nt_count += 1
+    elif action_type == SHIFT:
+      assert(nt_index is None)
+      new_prev_word_index = action_index
+      new_term_count += 1
+      new_cons_nt_count = 0
+    else:
+      assert(action_type == CLOSE_NT)
+      assert(beam_state.open_nt_count > 0) # is something to close
+      if beam_state.open_nt_count == 1: # check to make sure there are no remaining words
+        assert(beam_state.term_count == len_terms)
+      assert(nt_index == beam_state.open_nt_stack.car)  # make sure we match the opening terminal
+      new_open_nt_stack = beam_state.open_nt_stack.cdr
+      new_open_nt_count -= 1
+      new_cons_nt_count = 0
+      assert(new_open_nt_count >= 0)
+      assert((new_open_nt_stack is None) == (new_open_nt_count == 0))
+
+    new_state = beam_state._replace(
+      prev_beam_state=beam_state,
+      # lstm_state stays the same, is updated by score_actions
+      prev_word_index=new_prev_word_index,
+      open_nt_stack=new_open_nt_stack,
+      open_nt_count=new_open_nt_count,
+      cons_nt_count=new_cons_nt_count,
+      action_count=beam_state.action_count + 1,
+      term_count=new_term_count,
+      prev_action_type=action_type,
+      prev_action_index=action_index,
+      action_score=beam_state.score + action_score
+    )
+    return new_state
+
+
   def valid_actions(self, beam_state, term_ids, len_terms):
     # return list of valid (action_type, nt_index, action_index) for the current beam_state
     remaining_terms = len_terms - beam_state.term_count
@@ -57,7 +131,7 @@ class BeamSearch(object):
       actions.append((SHIFT, None, term_ids[beam_state.term_count]))
     # check close action: can't close immediately after an open, can't close a top level paren unless we've exhausted all remaining terms
     if beam_state.prev_action_type != OPEN_NT and (beam_state.open_nt_count > 1 or remaining_terms == 0):
-        actions.append((CLOSE_NT, beam_state.open_nt_stack.car, self.close_indices[beam_state.open_nt_stack.car]))
+      actions.append((CLOSE_NT, beam_state.open_nt_stack.car, self.close_indices[beam_state.open_nt_stack.car]))
     return actions
 
   def beam_search(self, term_ids, beam_size):
@@ -69,69 +143,9 @@ class BeamSearch(object):
 
     len_terms = len(term_ids)
 
-    def score_actions(beam_state):
-      feed_dict = {
-        self.model.input_data: np.array([[current_beam_item.prev_action_index]])
-      }
-      for (state_c, state_h), (c, h) in zip(current_beam_item.lstm_state, self.model.initial_state):
-        feed_dict[c] = state_c
-        feed_dict[h] = state_h
-      fetches = [self.model.log_probs]
-      for c, h in self.model.final_state:
-        fetches.append(c)
-      res = self.session.run(fetches, feed_dict)
-      log_probs = res[0]
-      lstm_state_flat = res[1:] # [c1, h1, c2, h2...]
-      lstm_state = [state_flat[i:i+2] for i in range(0, len(lstm_state_flat), 2)]
-      new_state = beam_state._replace(lstm_state=lstm_state)
-      return log_probs, new_state
-
-    def choose_action(beam_state, action_type, nt_index, action_index, action_score):
-      new_prev_word_index = beam_state.prev_word_index
-      new_open_nt_stack = beam_state.open_nt_stack
-      new_open_nt_count = beam_state.open_nt_count
-      new_cons_nt_count = beam_state.cons_nt_count
-      new_term_count = beam_state.term_count
-
-      if action_type == OPEN_NT:
-        new_open_nt_stack = ConsCell(nt_index, beam_state.open_nt_stack)
-        new_open_nt_count += 1
-        new_cons_nt_count += 1
-      elif action_type == SHIFT:
-        assert(nt_index is None)
-        new_prev_word_index = action_index
-        new_term_count += 1
-        new_cons_nt_count = 0
-      else:
-        assert(action_type == CLOSE_NT)
-        assert(beam_state.open_nt_count > 0) # is something to close
-        if beam_state.open_nt_count == 1: # check to make sure there are no remaining words
-          assert(beam_state.term_count == len_terms)
-        new_open_nt_stack = new_open_nt_stack.cdr
-        new_open_nt_count -= 1
-        new_cons_nt_count = 0
-        assert(new_open_nt_count >= 0)
-        assert((new_open_nt_stack is None) == (new_open_nt_count == 0))
-
-      new_state = beam_state._replace(
-        prev_beam_state=beam_state,
-        # lstm_state stays the same, is updated by score_actions
-        prev_word_index=new_prev_word_index,
-        open_nt_stack=new_open_nt_stack,
-        open_nt_count=new_open_nt_count,
-        cons_nt_count=new_cons_nt_count,
-        action_count=beam_state.action_count + 1,
-        term_count=new_term_count,
-        prev_action_type=action_type,
-        prev_action_index=action_index,
-        action_score=beam_state.score + action_score
-      )
-      return new_state
-
-
     initial_beam_state = BeamState(prev_beam_state=None,
                                    lstm_state=initial_lstm_state,
-                                   prev_word_index=self.eos_index,
+                                   prev_word_index=None,
                                    open_nt_stack=None,
                                    open_nt_count=0,
                                    cons_nt_count=0,
@@ -141,36 +155,36 @@ class BeamSearch(object):
                                    prev_action_index=self.eos_index,
                                    score=0)
 
-    beam = []
+    # list of beam states
+    beam = [initial_beam_state]
+    # list of beam states
     completed = []
 
+    def successor_scoring_fn(action_score, action_type, nt_index, action_index, old_beam_state):
+      return action_score + old_beam_state.score
+
     while len(completed < beam_size and beam):
+      # list of (action_score, action_type, nt_index, action_index, old beam_state)
       successors = []
 
       while beam:
-        current_beam_item = beam.pop()
+        current_beam_state = beam.pop()
+        action_scores = self.score_actions(current_beam_state)
 
-        for actions in self.valid_actions(current_beam_item, term_ids, len_terms):
+        for (action_type, nt_index, action_index) in self.valid_actions(current_beam_state, term_ids, len_terms):
+          action_score = action_scores[action_index]
+          successors.append((action_score, action_type, nt_index, action_index, current_beam_state))
 
+      for (action_score, action_type, nt_index, action_index, old_beam_state) in heapq.nlargest(successors, key=successor_scoring_fn):
+        new_beam_state = self.choose_action(old_beam_state, action_type, nt_index, action_index, action_score, len_terms)
+        if new_beam_state.open_nt_count == 0:
+          assert(new_beam_state.term_count == len_terms)
+          completed.append(new_beam_state)
+        else:
+          beam.append(new_beam_state)
 
-    for i in range(len(x) - 1):
-      input_arr = x[i:i+1][:,None]
-      target_arr = x[i+1:i+2][:,None]
-      fetches = [self.model.log_probs]
-      for c, h in self.model.final_state:
-        fetches.append(c)
-        fetches.append(h)
-      feed_dict = {
-        self.model.input_data: input_arr
-      }
-      for (state_c, state_h), (c, h) in zip(state, self.model.initial_state):
-        feed_dict[c] = state_c
-        feed_dict[h] = state_h
-      res = self.session.run(fetches, feed_dict)
-      cost = res[0]
-      costs.append(cost[0])
-      state_flat = res[1:] # [c1, h1, c2, h2...]
-      state = [state_flat[i:i+2] for i in range(0, len(state_flat), 2)]
-      total_cost += np.sum(cost)
-    print(costs)
-    return total_cost
+    best_state = max(completed, key=lambda beam_state: beam_state.score)
+
+    best_action_indices, best_word_indices = backchain_beam_state(best_state)
+    assert(best_word_indices == term_ids)
+    return best_action_indices
